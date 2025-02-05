e5a54e00e6d77657a4008378db87bac6e5bed567
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2025-02-05 23:45:38 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TELEGRAM_TOKEN: 'VOTRE_TOKEN',
+    OWNER_ID: 'ID_ADMIN_TELEGRAM',
+    WA_SESSION_PATH: './sessions/',
+    PREMIUM_CMDS: ['xandroid', 'xios', 'xfreeze', 'xbugv1', 'xbugv2']
+};
